e729a6c50d20f20af9b900b213f08552448b29a2
danas <danas@asb9838nu-a13.csil.sfu.ca>
2015-03-02 19:19:05 -0800

    SUCK IT ZACH

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 void *fnC() {
@@ -7,7 +8,7 @@ void *fnC() {
     }   
 }
 
-
+/* For the PThread Mutex Test */
 pthread_mutex_t count_mutex;
 
 void *pthreadMutexTest() {
@@ -19,15 +20,30 @@ void *pthreadMutexTest() {
 	}   
 }
 
+/* For the PThread Mutex Test */
+pthread_spinlock_t count_spinlock;
+
+void *pthreadSpinlockTest() {
+	int i;
+	printf("Starting Test 2\n");
+	for(i=0;i<numItterations;i++) { 
+		pthread_spin_lock(&count_spinlock);
+		c++;
+		printf("Iteration Number: %d\n", c);
+		pthread_spin_unlock(&count_spinlock);	
+	}   
+}
+
+/* Run the tests */
 int runTest(int testID) {
 
-	/*You must create all data structures and threads for you experiments*/
-
-	if (testID == 0 || testID == 1 ) {/*Pthread Mutex*/
+	/*Pthread Mutex*/
+	if (testID == 0 || testID == 1 ) {
+		printf("Start of TestID 1 - Pthread Mutex\n");
 		c=0;
 		struct timespec start;
 		struct timespec stop;
-		unsigned long long result; //64 bit integer
+		unsigned long long result;
 
 		pthread_t *threads = (pthread_t* )malloc(sizeof(pthread_t)*numThreads);	
 		int i;
@@ -36,13 +52,12 @@ int runTest(int testID) {
 		clock_gettime(CLOCK_MONOTONIC, &start);
 		for(i=0;i<numThreads;i++) {
 	
-			if( rt=(pthread_create( threads+i, NULL, &pthreadMutexTest, NULL)) ) {
+			if( rt=(pthread_create(threads+i, NULL, &pthreadMutexTest, NULL)) ) {
 				printf("Thread creation failed: %d\n", rt);
 				return -1;	
 			}
 		}
 	
-		/* Wait for all threads to finish */
 		for(i=0;i<numThreads;i++) {
 			 pthread_join(threads[i], NULL);
 		}
@@ -50,35 +65,65 @@ int runTest(int testID) {
 
 		printf("Threaded Run Pthread (Mutex) Total Count: %d\n", c);
 		result=timespecDiff(&stop,&start);
-		printf("Pthread Mutex time(ms): %llu\n",result/1000000);
+		printf("Pthread Mutex time(ms): %llu\n\n",result/1000000);
 	}
 
 	/* Pthread Spinlock */
-	else if(testID == 0 || testID == 2) {
-	/*Pthread Spinlock goes here*/
+	if(testID == 0 || testID == 2) {
+		printf("Start of TestID 2 - Pthread Spinlock\n");
+		c=0;
+		struct timespec start;
+		struct timespec stop;
+		unsigned long long result;
+
+		pthread_t *threads = (pthread_t* )malloc(sizeof(pthread_t)*numThreads);	
+		int i;
+		int rt;
+
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		for(i=0;i<numThreads;i++) {
+	
+			if( rt=(pthread_create(threads+i, NULL, &pthreadSpinlockTest, NULL)) ) {
+				printf("Thread creation failed: %d\n", rt);
+				return -1;	
+			}
+		}
+	
+		for(i=0;i<numThreads;i++) {
+			 pthread_join(threads[i], NULL);
+		}
+		clock_gettime(CLOCK_MONOTONIC, &stop);
+
+		printf("Threaded Run Pthread (Spinlock) Total Count: %d\n", c);
+		result=timespecDiff(&stop,&start);
+		printf("Pthread Spinlock time(ms): %llu\n",result/1000000);
 	}
 
-	/*MySpinlockTAS*/
-	else if(testID == 0 || testID == 3) {
-	/* mySpinlock TAS goes here*/
+	/* MySpinlockTAS */
+	if(testID == 0 || testID == 3) {
+
 	}
 
-	/*....you must implement the other tests....*/
+	/* MySpinlockTTAS */
+	if(testID == 0 || testID == 4) {
+
+	}
+
+	/* MyMutexTAS */
+	if(testID == 0 || testID == 5) {
+
+	}
+
+	/* MyMutexTTAS */
+	if(testID == 0 || testID == 6) {
+
+	}
 
 	return 0;
 }
 
-int testAndSetExample() {
-	/* Test is set to 0 */
-	volatile long test = 0;
-	printf("Test before atomic OP:%d\n",test);
-	tas(&test);
-	printf("Test after atomic OP:%d\n",test);
-}
-
+/* Process user input */
 int processInput(int argc, char *argv[]) {
-
-/*testid: 0=all, 1=pthreadMutex, 2=pthreadSpinlock, 3=mySpinLockTAS, 4=mySpinLockTTAS, 5=myMutexTAS, 6=myMutexTTAS*/
 
 	char buffer[512];
 	char *pointer = buffer;
@@ -92,32 +137,49 @@ int processInput(int argc, char *argv[]) {
 	numItterations=1000000;
 	testID=0;
 
-	printf("buffer: %s\n", buffer);
-	command = strtok_r(pointer, " \n\t", &pointer);
-	printf("buffer: %s\n", buffer);
-	printf("command: %s\n", command);
-
 	while (command = strtok_r(pointer, " \n\t", &pointer)) {
 		
-		//if (strcmp(command, "-t") == 0) {
-			//command = strtok_r(pointer, " \n\t", &pointer);
-			//numThreads = atoi(command);
-		//}
+		if (strcmp(command, "-t") == 0) {
+			command = strtok_r(pointer, " \n\t", &pointer);			
+			if (atoi(command) > 0) {
+				numThreads = atoi(command);
+			} else {
+				printf("Invalid Thread count. Thead count must be an integer greater than 0.\n");
+			}
+		} else if (strcmp(command, "-i") == 0) {
+			command = strtok_r(pointer, " \n\t", &pointer);
+			if (atoi(command) > 0) {
+				numItterations = atoi(command);
+			} else {
+				printf("Invalid Iteration count. Iteration count must be an integer greater than 0.\n");
+			}
+		} else if (strcmp(command, "-d") == 0) {
+			command = strtok_r(pointer, " \n\t", &pointer);
+			if (atoi(command) <= 6 && atoi(command) >= 0) {
+				testID = atoi(command);
+			} else {
+				printf("Invalid TestID. Tedt ID must be between 0 and 6\n");
+			}
+		} else {
+			printf("Command '%s' not recognized.\n", command);
+		}
 	}
+
+	printf("Number of threads: %d\n", numThreads);
+	printf("Number of ierations: %d\n", numItterations);
+	printf("Test ID: %d\n\n", testID);
 	
 	return 0;
 }
 
-
+/* Main Program */
 int main(int argc, char *argv[]) {
 
 
 	printf("\nUsage of: %s \n", argv[0]);
-	printf("Please use the following format: -t #threads -i #itterations -d testid\n", argv[0]);
+	printf("Please use the following input format: -t #threads -i #itterations -d testid\n", argv[0]);
 	printf("If a parameter is missed default values will be used. \n", argv[0]);
-	printf("testid: 0=all, 1=pthreadMutex, 2=pthreadSpinlock, 3=mySpinLockTAS, 4=mySpinLockTTAS, 5=myMutexTAS, 6=myMutexTTAS\n");	
-	
-	//testAndSetExample(); //Uncomment this line to see how to use TAS
+	printf("testid:\n    0 = all\n    1 = pthreadMutex\n    2 = pthreadSpinlock\n    3 = mySpinLockTAS\n    4 = mySpinLockTTAS\n    5 = myMutexTAS\n    6 = myMutexTTAS\n");	
 	
 	processInput(argc,argv);
 	runTest(testID);
